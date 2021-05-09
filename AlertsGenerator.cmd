aa5f7e40820038e1b76fd062867a6fc8fbc4b19a
Yaron Habot <>
2021-05-09 12:45:24 -0400

    add -k to curl scripts to prevent curl complaint about insecure TLS mode

--- AlertsGenerator.cmd
+++ AlertsGenerator.cmd
@@ -1,7 +1,7 @@
-curl -X POST "https://localhost:5001/NCDB" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"111223333\",\"message\":\"Criminal\"}"
+curl -X POST "https://localhost:5001/NCDB" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"111223333\",\"message\":\"Criminal\"}"  -k
 
 TIMEOUT  2
-curl -X POST "https://localhost:5001/CB" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"111224444\",\"message\":\"Bankrupt\"}"
+curl -X POST "https://localhost:5001/CB" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"111224444\",\"message\":\"Bankrupt\"}" -k
 
 TIMEOUT  2
-curl -X POST "https://localhost:5001/CB" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"111224444\",\"message\":\"Debt\"}"
+curl -X POST "https://localhost:5001/CB" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"111224444\",\"message\":\"Debt\"}" -k
--- Tests.cmd
+++ Tests.cmd
@@ -1,29 +1,29 @@
 echo Employee get valid SSN
-curl -X GET "https://localhost:5001/Employee?ssn=111223333" -H  "accept: text/plain"
+curl -X GET "https://localhost:5001/Employee?ssn=111223333" -H  "accept: text/plain" -k
 
 echo Employee get invalid SSN
-curl -X GET "https://localhost:5001/Employee?ssn=1112" -H  "accept: text/plain"
+curl -X GET "https://localhost:5001/Employee?ssn=1112" -H  "accept: text/plain" -k
 
 echo Employee update valid SSN
-curl -X PUT "https://localhost:5001/Employee" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"111223333\",\"firstName\":\"Yaron\",\"lastName\":\"Habot\",\"address\":\"1 Main st.\",\"city\":\"Orlando\",\"state\":\"FL\"}"
+curl -X PUT "https://localhost:5001/Employee" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"111223333\",\"firstName\":\"Yaron\",\"lastName\":\"Habot\",\"address\":\"1 Main st.\",\"city\":\"Orlando\",\"state\":\"FL\"}" -k
 
 echo Employee update invalid SSN
-curl -X PUT "https://localhost:5001/Employee" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"1112\",\"firstName\":\"Yaron\",\"lastName\":\"Habot\",\"address\":\"1 Main st.\",\"city\":\"Orlando\",\"state\":\"FL\"}"
+curl -X PUT "https://localhost:5001/Employee" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"1112\",\"firstName\":\"Yaron\",\"lastName\":\"Habot\",\"address\":\"1 Main st.\",\"city\":\"Orlando\",\"state\":\"FL\"}" -k
 
 echo Employee insert valid SSN
-curl -X POST "https://localhost:5001/Employee" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"111223333\",\"firstName\":\"Yaron\",\"lastName\":\"Habot\",\"address\":\"1 Main st.\",\"city\":\"Orlando\",\"state\":\"FL\"}"
+curl -X POST "https://localhost:5001/Employee" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"111223333\",\"firstName\":\"Yaron\",\"lastName\":\"Habot\",\"address\":\"1 Main st.\",\"city\":\"Orlando\",\"state\":\"FL\"}" -k
 
 echo Employee insert invalid SSN
-curl -X POST "https://localhost:5001/Employee" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"1112\",\"firstName\":\"Yaron\",\"lastName\":\"Habot\",\"address\":\"1 Main st.\",\"city\":\"Orlando\",\"state\":\"FL\"}"
+curl -X POST "https://localhost:5001/Employee" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"1112\",\"firstName\":\"Yaron\",\"lastName\":\"Habot\",\"address\":\"1 Main st.\",\"city\":\"Orlando\",\"state\":\"FL\"}" -k
 
 echo Employee delete valid SSN
-curl -X DELETE "https://localhost:5001/Employee?ssn=111223333" -H  "accept: text/plain"
+curl -X DELETE "https://localhost:5001/Employee?ssn=111223333" -H  "accept: text/plain" -k
 
 echo Employee delete invalid SSN
-curl -X DELETE "https://localhost:5001/Employee?ssn=1112" -H  "accept: text/plain"
+curl -X DELETE "https://localhost:5001/Employee?ssn=1112" -H  "accept: text/plain" -k
 
 echo National Crime Database
-curl -X POST "https://localhost:5001/NCDB" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"111223333\",\"message\":\"Criminal\"}"
+curl -X POST "https://localhost:5001/NCDB" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"111223333\",\"message\":\"Criminal\"}" -k
 
 echo Credit Bureau
-curl -X POST "https://localhost:5001/CB" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"111223333\",\"message\":\"Bankrupt\"}"
+curl -X POST "https://localhost:5001/CB" -H  "accept: text/plain" -H  "Content-Type: application/json" -d "{\"ssn\":\"111223333\",\"message\":\"Bankrupt\"}" -k
